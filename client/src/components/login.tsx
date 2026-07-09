e890463d8b91cb603b73d51d5931d158308773e3
ampardo18 <apardo@hawk.iit.edu>
2026-07-09 18:39:17 -0500

    finished login and registration components; backend logic for the register and sign in buttons will be added soon

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,11 +1,59 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import '../styles/globals.css'
 
 function Login(){
     const navigate = useNavigate()
 
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target
+        setFormData({
+            ...formData,
+            [name] : value
+        })
+    }
+
+
+
     return(
         <div className='min-h-screen select-none px-4 py-6'>
-            Login
+            <form className='flex flex-col space-y-4 p-4 bg-gray-700 rounded-lg shadow-md max-w-md w-full mx-auto mt-20'>
+                <h2 className='text-l font-bold text-center'>Log-in</h2>
+                <div className='flex flex-col'>
+                    <label>Email</label>
+                    <input
+                        type='email'
+                        name='email'
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                        className='w-full border rounded p-1'
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label>Password</label>
+                    <input
+                        type='password'
+                        name='password'
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                        className='w-full border rounded p-1'
+                    />
+                </div>
+                <button type='submit' className='main-buttons'>Sign-in</button>
+                <div className="flex items-center my-2">
+                    <div className="flex-1 h-px bg-white" />
+                    <span className="px-2 text-sm text-white">or</span>
+                    <div className="flex-1 h-px bg-white" />
+                </div>
+                <Link to='/register' className='text-blue-500 hover:text-blue-400 underline text-center'>Don't have an account? Register Now</Link>
+            </form>
         </div>
     )
 }
--- client/src/components/register.tsx
+++ client/src/components/register.tsx
@@ -1,10 +1,74 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import React, {useState} from 'react'
 
 function Register(){
     const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        name: '',
+        encryption_key: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const {name, value} = e.target
+        setFormData({
+            ...formData,
+            [name] : value
+        })
+    }
 
     return(
         <div className='min-h-screen select-none px-4 py-6'>
+            <form className='flex flex-col space-y-4 p-4 bg-gray-700 rounded-lg shadow-md max-w-md w-full mx-auto mt-20'>
+                <h2 className='font-bold text-l text-center'>Register</h2>
+                <div className="flex flex-col">
+                    <label>Email</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                        className="w-full border rounded p-1"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label>Password</label>
+                    <input 
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                        className="w-full border rounded p-1"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label>Name</label>
+                    <input 
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                        className="w-full border rounded p-1"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label>Encryption Key</label>
+                    <input 
+                        type="password"
+                        name="firstname"
+                        value={formData.encryption_key}
+                        onChange={handleChange}
+                        required
+                        className="w-full border rounded p-1"
+                    />
+                </div>
+                <button type='submit' className='main-buttons'>Register</button>
+
+            </form>
 
         </div>
     )
